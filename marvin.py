0e770b5626db0dcde5ec431f99cf6c0256a8905f
KKallas <kaspar@digitalsputnik.com>
2023-05-05 10:45:21 +0300

    removed the token

--- marvin.py
+++ marvin.py
@@ -2,7 +2,7 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = 'MTA4NTMyMjE1MTU4MzgyNTk1MQ.GUXsn2.cX_MXeuiU6JnpmuxoF-XW1eQ6OU1MCe5OcVfSM'
+TOKEN = ''
 
 client = discord.Client()
 
