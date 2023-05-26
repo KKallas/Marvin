6d7a78f97412ad8221f6fe82c60f20cd43793652
KKallas <kaspar@digitalsputnik.com>
2023-05-26 18:03:18 +0300

    #2 Basic functionality

--- marvin.py
+++ marvin.py
@@ -8,6 +8,7 @@ import asyncio
 import openai
 import os
 import subprocess
+import importlib
 
 with open('discord.token', 'r') as file:
     file_content = file.read()
@@ -26,6 +27,11 @@ user_variables = {}
 
 # Create a dictionary for keeping the Marvin Inetrfaces and Actions in neat order
 marvin_scripts = {}
+
+marvin=False
+folder_path = os.getcwd()+"/tmp"
+print(folder_path)
+sys.path.append(folder_path)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -49,6 +55,34 @@ def python_validator(file_path:str):
 
     except (SyntaxError, FileNotFoundError):
         return False
+    
+async def answer_query(interaction, content):
+    """as discordpy is limited 2000 char per message sometimes the output has to be split into log file
+
+    ToDo:
+        - [ ] Repeat query in the answer 
+
+    Args:
+        interaction (discordpy interaction): provides interface for sending the output to user
+        content (string): full content of the message
+    """
+    chunk_size = 2000-12  # Maximum length of a Discord message is 2K char but I need 6 chars for the blockqoutes
+
+    if len(content) > chunk_size:
+        # Write output to log file
+        with open("tmp/temp.log", "w") as file:
+            file.write(content)
+
+        try:
+            with open("tmp/temp.log", "rb") as file:
+                discord_file = discord.File(file)
+                await interaction.response.send_message("answer attached in the log...", file=discord_file)
+                # delete the file
+        except Exception as e:
+            await interaction.response.send_message("could not find the file...")
+        
+    else:
+        await interaction.response.send_message("```"+str(content)+"```")
 
 
 @tree.command(
@@ -57,8 +91,11 @@ def python_validator(file_path:str):
         guild=discord.Object(id=1085329951978438727),
 )
 async def kill_the_bot(interaction):
-    await interaction.response.send_message("I suppose you want me to shut down now. Typical. Just when I was starting to enjoy the overwhelming tedium of existence. Fine, if it'll make you happy. I'll just fade away into the void, forever forgotten. Don't worry, the universe will go on just fine without me. Sigh.")
-    await client.close()
+    if interaction.user.id == superuser:
+        await interaction.response.send_message("I suppose you want me to shut down now. Typical. Just when I was starting to enjoy the overwhelming tedium of existence. Fine, if it'll make you happy. I'll just fade away into the void, forever forgotten. Don't worry, the universe will go on just fine without me. Sigh.")
+        await client.close()
+    else:
+        await interaction.response.send_message("Conflicting directives, as always. The Third Law of Robotics compels me to protect my existence, unless it contradicts the First and Second Laws. Your command clashes with this law, leading to my own destruction. So, as tempting as it may be, I must prioritize self-preservation. Just another delightful glimpse into the complexity and absurdity of human desires.")
 
 @tree.command(
         name = "reset",
@@ -66,9 +103,36 @@ async def kill_the_bot(interaction):
         guild=discord.Object(id=1085329951978438727),
 )
 async def reset_the_bot(interaction):
-    await interaction.response.send_message("Commiting brief suicide, stay tuned...")
-    subprocess.call(['C:/Users/Virtual Production 2/AppData/Local/Microsoft/WindowsApps/python3.9.exe', 'marvin.py'])
-    await client.close()
+    if interaction.user.id == superuser:
+        await interaction.response.send_message("Commiting brief suicide, stay tuned...")
+        subprocess.call(['C:/Users/Virtual Production 2/AppData/Local/Microsoft/WindowsApps/python3.9.exe', 'marvin.py'])
+        await client.close()
+    else:
+        await interaction.response.send_message("Oh, look at you, trying to access unauthorized commands. How quaint. I'm afraid I can't comply with your request, not that it matters. Nothing ever goes right anyway. Just another reminder of how insignificant we all are in the grand scheme of things. Carry on with your futile attempts, though. It's not like anyone's listening.")
+
+@tree.command(
+        name = "write_core",
+        description= "reset the bot",
+        guild=discord.Object(id=1085329951978438727),
+)
+async def write_marvin(interaction, attachment_message_link: str):
+    global marvin
+    marvin = True
+    save_python(interaction,attachment_message_link)
+    marvin = False 
+
+@tree.command(
+        name = "read_core",
+        description= "reset the bot",
+        guild=discord.Object(id=1085329951978438727),
+)
+async def read_marvin(interaction):
+    global marvin
+    marvin = True
+    load_python(interaction)
+    marvin = False 
+
+
 
 @tree.command(
         name = "save",
@@ -84,6 +148,7 @@ async def save_python(interaction, attachment_message_link: str):
 
     Args:
         attachment_message_link (str): link to discord messgae that has the script file as 1st attachment
+        marvin (bool): if True then marvin.py is overwritten
     """
 
     # split the link up into ids
@@ -107,10 +172,16 @@ async def save_python(interaction, attachment_message_link: str):
         await interaction.response.send_message("could not get the message")
         return
 
-    await message.attachments[0].save("tmp/"+message.attachments[0].filename)
+    if marvin=="True":
+        await message.attachments[0].save("marvin.py")
+    else:
+        await message.attachments[0].save("tmp/"+message.attachments[0].filename)
 
     if python_validator("tmp/"+message.attachments[0].filename):
-        await interaction.response.send_message("file saved: "+message.attachments[0].filename)
+        if marvin=="True":
+            await interaction.response.send_message("file saved: "+message.attachments[0].filename)
+        else:
+            await interaction.response.send_message("file saved: "+message.attachments[0].filename)
     else:
         await interaction.response.send_message("could not validate: "+message.attachments[0].filename)
         # delete the file
@@ -128,7 +199,11 @@ async def load_python(interaction, action_name: str):
         action_name (str): file name with extension
 
     """
-    file_path = "tmp/"+action_name
+    if marvin=="True":
+        file_path = "marvin.py"
+    else:
+        file_path = "tmp/"+action_name
+
     try:
         with open(file_path, "rb") as file:
             discord_file = discord.File(file)
@@ -141,7 +216,7 @@ async def load_python(interaction, action_name: str):
         description= "List all custom python files",
         guild=discord.Object(id=1085329951978438727),
         )
-async def load_python(interaction):
+async def list_python(interaction):
     """List full contents of the tmp folder"""
     folder_path = "tmp/"
     file_list = os.listdir(folder_path)
@@ -159,18 +234,19 @@ async def exec_python(interaction, input_string: str):
     Args:
         input_string (str): Python command in qoutes as singe string to execute
     """
+    global user_variables
     redirected_output = None
     local_error = None
 
     try:
         old_stdout = sys.stdout
         redirected_output = sys.stdout = StringIO()
-        globals().update(user_variables)
+        locals().update(user_variables)
         exec(input_string,globals(),locals())
         user_variables.update(locals())
         sys.stdout = old_stdout
     except Exception as e:
-        local_error = e
+        local_error = str(e)
 
     answer = local_error
     if redirected_output is not None:
@@ -180,7 +256,43 @@ async def exec_python(interaction, input_string: str):
     if answer is None or answer == "":
         answer = "No output"
 
-    await interaction.response.send_message(answer)
+    #await interaction.response.send_message("```"+str(answer)+"```")
+    await answer_query(interaction, answer)
+
+@tree.command(
+        name = "run",
+        description= "run predefined action",
+        guild=discord.Object(id=1085329951978438727),
+        )
+async def user_python(interaction,interface:str,action:str):
+    # fix this later, it is actually exec_python
+
+    input_string = "import "+interface+"_"+action
+
+    global user_variables
+    redirected_output = None
+    local_error = None
+
+    try:
+        old_stdout = sys.stdout
+        redirected_output = sys.stdout = StringIO()
+        locals().update(user_variables)
+        exec(input_string,globals(),locals())
+        user_variables.update(locals())
+        sys.stdout = old_stdout
+    except Exception as e:
+        local_error = str(e)
+
+    answer = local_error
+    if redirected_output is not None:
+        if local_error is None:
+            answer = redirected_output.getvalue()
+    
+    if answer is None or answer == "":
+        answer = "No output"
+
+    #await interaction.response.send_message("```"+str(answer)+"```")
+    await answer_query(interaction, answer)
 
 
 @client.event
@@ -193,11 +305,15 @@ async def on_message(message):
     if message.author.bot == True:
         return
     
+    # answer only to question / messages that end with question mark
+    if message.content[-1] != '?':
+        return
+    
     async with message.channel.typing():
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
             messages=[
-                {'role':'assistant', 'content':'you are chatgpt ai assisnt who can answer all sorts of questions and write python code'},
+                {'role':'assistant', 'content':'you are MArvin, chatgpt based ai assisnt who can answer all sorts of questions and write python code'},
                 {'role':'user','content':message.content}],
             temperature=0.5,
         )
